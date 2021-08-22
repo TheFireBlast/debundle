f9726d73e08b39204077621fad756ebf0cc1498f
TheFireBlast <fireblast901@gmail.com>
2021-08-22 20:46:55 -0300

    Improve ExtendedError and parseBundleModules

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ import * as ESTree from "estree";
 import * as cliHighlight from "cli-highlight";
 
 import type { Bundle } from "./subcomponents/Bundle";
-import { Module } from "./subcomponents/Module";
+import type { Module } from "./subcomponents/Module";
 
 export function highlight(code: string, language = "javascript") {
     return cliHighlight.highlight(code, {
@@ -15,16 +15,12 @@ export function highlight(code: string, language = "javascript") {
 }
 
 export class ExtendedError extends Error {
-    context: any;
-    constructor(name: string, message: string, ...args: any[] /*, context */) {
-        let context = {};
-        if (typeof args[args.length - 1] !== "string") {
-            context = args.pop();
-        }
-        let description = args.join("\n");
+    constructor(name: string, message: string, description?: string, context?: { node?: ESTree.Node; module?: Module }) {
         super([message, ...(description ? [`\nDetails: ${description}`] : []), `\nContext: ${JSON.stringify(context)}`].join("\n"));
         this.name = name;
-        this.context = context;
+        this.message = message;
+        if (context?.module) this.message += ` (@${context.module.path} | id:${context.module.id})`;
+        if (context?.node) this.message += `\n${highlight(generate(context.node)).replace(/^/g, "  ")}`;
     }
 }
 
@@ -33,28 +29,20 @@ export function cloneAst(ast: ESTree.Node) {
     return (acorn.parse("var a = " + generate(ast), { ecmaVersion: 2020 }) as any).body[0].declarations[0].init;
 }
 
-export function parseBundleModules(node: ESTree.Node, bundle: Bundle, isChunk = false) {
+export function parseBundleModules(node: ESTree.Node, bundle: Bundle, isChunk = false): [number | string, ESTree.Node][] {
     if (node.type === "ObjectExpression") {
-        // Object
-        return node.properties.map((property) => {
-            //@ts-ignore
-            const key = typeof property.key.value !== "undefined" ? property.key.value : property.key.name;
-            //@ts-ignore
-            return [key, property.value];
-        });
+        return node.properties.map((property: ESTree.Property) => [property.key["value"] ?? property.key["name"], property.value]);
     } else if (node.type === "ArrayExpression") {
-        // Array
         return node.elements.map((moduleAst, moduleId) => [moduleId, moduleAst]);
     } else {
         throw new ExtendedError(
             "BundleModuleParsingError",
-            "Cannot locate modules within bundle - it is not an array or an object!",
-            "The module bootstrapping function was found and parsed, but no array or object",
-            "containing module closures was found. This probably means that the module being parsed",
-            "is something a bit unusual, and in order to unpack this bundle, a manual path to the",
-            'module array must be specified by adding a "moduleClosurePath" key to the "options" object',
-            `in the ${bundle.metadataFilePath} file that was created. For more information, see [INSERT LINK HERE].`,
-            { foo: true }
+            `Cannot locate modules within bundle - it is not an array or an object!
+The module bootstrapping function was found and parsed, but no array or object\
+containing module closures was found. This probably means that the module being parsed\
+is something a bit unusual, and in order to unpack this bundle, a manual path to the\
+module array must be specified by adding a "moduleClosurePath" key to the "options" object\
+in the "${bundle.metadataFilePath}" file that was created` //. For more information, see [INSERT LINK HERE].`,
         );
     }
 }
@@ -72,9 +60,9 @@ export function oxford(arr: string[], conjunction: string = "and", ifempty: stri
 export function assertType<T extends string>(type: string, expected: T | T[], print?: ESTree.Node, module?: Module): asserts type is T {
     expected = [expected].flat(3) as T[];
     if (!expected.includes(type as T)) {
-        var info = ''
-        if (print) info = `${highlight(generate(print)).replace(/^/g,'  ')}`;
-        if (module) info = `at ${module.path} (${module.id})\n${info}`;
-        throw new ExtendedError(`Expected ${oxford(expected, "or", "nothing")}, got ${type} instead`,info);
+        throw new ExtendedError("TypeAssertionError", `Expected ${oxford(expected, "or", "nothing")}, got ${type} instead`, null, {
+            node: print,
+            module,
+        });
     }
 }
