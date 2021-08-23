ee9fad01c4836754f6c13801ff631077a642d1d2
TheFireBlast <fireblast901@gmail.com>
2021-08-22 21:58:27 -0300

    Add comments to JSXVisitor and fix a few issues

--- src/subcomponents/JSXVisitor.ts
+++ src/subcomponents/JSXVisitor.ts
@@ -3,7 +3,7 @@ import { generate } from "./astring-jsx";
 import type * as ESTree from "estree";
 import type {} from "./estree-override";
 import * as jsxKeys from "estraverse-fb/keys";
-import type { JSXSpreadAttributeKind } from "ast-types/gen/kinds";
+import type { JSXAttribute, JSXExpressionContainer, JSXSpreadAttribute } from "./JSXTypes";
 
 import { Module } from "./Module";
 import { assertType } from "../utils";
@@ -23,10 +23,14 @@ export function setCurrentModule(value: Module) {
     currentModule = value;
 }
 
+// Transforms plain JS back into JSX
+// Example:
+// JSX.jsxs(MyComponent, Object.assign({}, mySpreadAttr, {myNumProp: 123, myStrProp: "abc"}, {children: ["Hello world", JSX.jsx("div", {}, void 0)]}, void 0)
+// <MyComponent {...mySpreadAttr} myNumProp={123} myStrProp="abc">Hello world<div></div></MyComponent>
+//TODO: rename to JSXTransform
 export const JSXVisitor: estraverse.Visitor = {
     enter: function (node, parent) {
         if (isJsxCall(node)) {
-            // console.log('Found JSX call "%s"', highlight(generate(node)));
             let args = node.arguments;
             var jsxElement = {
                 type: "JSXElement",
@@ -41,31 +45,34 @@ export const JSXVisitor: estraverse.Visitor = {
                 },
                 closingElement: null,
                 children: [],
+                _parent: parent,
             };
 
+            // ObjectExpression containing the JSXElement's attributes
             var attrNode: ESTree.Node;
+            //TODO: support Object.assign for ES6+
+            // If the node is an `Object.assign` call, then the objects are merged
             if (args[1].type == "CallExpression" && args[1].callee.type == "Identifier" && args[1].callee.name == "__assign") {
                 let result = { type: "ObjectExpression", properties: [] } as ESTree.ObjectExpression;
-                let _props = new Map();
+                let _props: Map<string, ESTree.Property> = new Map();
                 for (let obj of args[1].arguments) {
                     assertType(obj.type, ["ObjectExpression", "Identifier"], args[1], currentModule);
+                    // If one the objects is an Identifier, then it's transformed into a JSXSpreadAttribute ({...abc})
                     if (obj.type == "Identifier") {
                         jsxElement.openingElement.attributes.push({
                             type: "JSXSpreadAttribute",
                             argument: obj,
-                        } as JSXSpreadAttributeKind);
-                    }
-                    if (obj.type == "ObjectExpression")
+                        } as JSXSpreadAttribute);
+                    } else if (obj.type == "ObjectExpression") {
                         for (let obj_prop of obj.properties) {
                             assertType(obj_prop.type, "Property", obj);
                             assertType(obj_prop.key.type, ["Identifier", "Literal"], obj);
                             if (obj_prop.key.type == "Identifier") _props.set(obj_prop.key.name, obj_prop);
-                            else _props.set(obj_prop.key.value, obj_prop);
+                            else _props.set(obj_prop.key.value as string, obj_prop);
                         }
+                    }
                 }
-                for (let p of _props.values()) {
-                    result.properties.push(p);
-                }
+                for (let p of _props.values()) result.properties.push(p);
                 attrNode = result;
             } else attrNode = args[1];
             if (attrNode.type == "ObjectExpression") {
@@ -73,29 +80,17 @@ export const JSXVisitor: estraverse.Visitor = {
                 for (let p of attrNode.properties) {
                     if (p.type != "Property" || p.key.type != "Identifier") continue;
                     if (p.key.name == "children") {
-                        if (p.value.type == "ArrayExpression") jsxElement.children = p.value.elements;
-                        else {
-                            if (p.value.type.startsWith("JSX")) {
-                                jsxElement.children.push(p.value);
+                        let children = (p.value.type == "ArrayExpression" ? p.value.elements : [p.value]) as ESTree.Expression[];
+                        for (let el of children) {
+                            if (el.type.startsWith("JSX")) {
+                                jsxElement.children.push(el);
                             } else {
                                 jsxElement.children.push({
                                     type: "JSXExpressionContainer",
-                                    expression: p.value,
+                                    expression: el,
                                 });
                             }
                         }
-                        // //@ts-ignore
-                        // if (p.value.type != "ArrayExpression") {
-                        //     //@ts-ignore
-                        //     p.value = {
-                        //         type: "ArrayExpression",
-                        //         elements: [p.value as any],
-                        //         _parent: attrNode,
-                        //     };
-                        // }
-                        // for (var child of p.value.elements) {
-                        //     jsxElement.children.push(child);
-                        // }
                         continue;
                     }
                     var attr = {
@@ -105,20 +100,22 @@ export const JSXVisitor: estraverse.Visitor = {
                             name: p.key.name,
                         },
                         value: null,
-                    };
+                    } as JSXAttribute;
                     if (p.value.type == "Literal" && (p.value.raw[0] == "'" || p.value.raw[0] == '"')) {
                         attr.value = p.value;
                     } else {
                         attr.value = {
                             type: "JSXExpressionContainer",
                             expression: p.value,
-                        };
+                        } as JSXExpressionContainer;
                     }
                     jsxElement.openingElement.attributes.push(attr);
                 }
             }
 
             if (jsxElement.children.length > 0) {
+                // There's no way to know if the original JSX was `<App></App>` or `<App/>`,
+                // so we only set `selfClosing` to false when there is at least one child.
                 jsxElement.openingElement.selfClosing = false;
                 jsxElement.closingElement = {
                     type: "JSXClosingElement",
@@ -129,9 +126,6 @@ export const JSXVisitor: estraverse.Visitor = {
                 };
             }
 
-            // console.log("Transformed to %s", highlight(generate(jsxElement)));
-
-            //@ts-ignore
             return jsxElement as ESTree.Node;
         }
     },
